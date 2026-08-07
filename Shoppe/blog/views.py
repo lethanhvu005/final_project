6d842fcecafeb7a0e48fe461bdc02632a336b67f
Typhoon Le <lethanhvu005@gmail.com>
2026-08-07 20:04:08 +0700

    update code

--- Shoppe/blog/views.py
+++ Shoppe/blog/views.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+from django.db.models.aggregates import Avg
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
@@ -9,6 +12,9 @@ def BlogMain(request):
     paginator = Paginator(blogs,3)
     page_number = request.GET.get('page')
     page_obj =  paginator.get_page(page_number)
+    for blog in page_obj:
+        rate_avg =Rate.objects.filter(blog=blog).aggregate(avg=Avg('rate'))['avg'] or 0
+        blog.rate_avg = rate_avg or 0
     return render(request , 'blog_main.html',{'page_obj':page_obj})
 def BlogDetail(request,id):
     blog = get_object_or_404(Blog,id=id)
@@ -21,14 +27,17 @@ def Rates(request):
         rate = request.POST.get('rate')
         id_blog = request.POST.get('id_blog')
         id_user = request.user
-        print ("----------------------------"+str(id_user))
         try:
             blog = Blog.objects.get(id= id_blog)
+            if Rate.objects.filter(blog=blog,user=id_user):
+                rate_avg = Rate.objects.filter(blog=blog).aggregate(avg=Avg('rate'))["avg"] or 0
+                return JsonResponse({'success':False,'err':'Bạn đã đánh giá rồi','rate_avg':rate_avg})
+            rate_avg = Rate.objects.filter(blog=blog).aggregate(avg=Avg('rate'))["avg"] or 0
             Rate.objects.create(rate=rate,blog=blog,user=id_user)
-            return JsonResponse({'success':True})
+            return JsonResponse({'success':True,'rate_avg':rate_avg})
         except Blog.DoesNotExist:
             return JsonResponse({'success':False,'err':'Blog Not Found'})
     return JsonResponse({'success':False,'err':'K thấy request nào'})
-        
+   
         
   
