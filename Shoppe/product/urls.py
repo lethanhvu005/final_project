b6bc37effdc00c73e04307791be0379e63063644
lethanhvu005 <165863692+lethanhvu005@users.noreply.github.com>
2026-08-24 21:16:53 +0700

    update code

--- Shoppe/product/urls.py
+++ Shoppe/product/urls.py
@@ -3,6 +3,9 @@ from . import views
 app_name ='product'
 urlpatterns = [
         path('my_product/',views.myProduct,name='my_product'),
-        path('add_product/',views.addProduct,name='add_product')
+        path('add_product/',views.addProduct,name='add_product'),
+        path('edit_product/<int:id>',views.editProduct,name='edit_product'),
+        path('delete_product/<int:id>',views.deleteProduct,name='delete_product')
+
     ]
     
--- Shoppe/product/views.py
+++ Shoppe/product/views.py
@@ -1,19 +1,18 @@
 import re
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.core.files.storage import default_storage
 from django import views
 from django.http import JsonResponse
 from django.urls import reverse
 from .models import Product
-from .forms import Product
+from .forms import ProductForm
 def myProduct(request):
     products = Product.objects.all()
     return render(request,'my_product.html',{'products':products})
 def addProduct(request):
     image_err =[]
     if request.method == "POST":
-        add_form = Product(request.POST)
+        add_form = ProductForm(request.POST)
         images = request.FILES.getlist('images')
         if not images:
             image_err.append('Cần chọn tối thiểu 1 ảnh')
@@ -35,10 +34,35 @@ def addProduct(request):
             product.save()
             return JsonResponse({'success':'Tạo thành công',"redirect_url": reverse("product:my_product")})   
     else:
-        add_form=Product()
+        add_form=ProductForm()
     return render(request,'add_product.html',{'add_form':add_form,'image_err':image_err,})
-def editProduct(request):
-    user = request.user
+def editProduct(request,id):
+    product = get_object_or_404(Product,id=id)
+    image_err =[]
     if request.method == 'POST':
-        product = Product(request.POST,instance=user)
-        return render(request,'edit_product.html', 'product':product)
+        product_edit = ProductForm(request.POST,instance=product,)
+        images = request.FILES.getlist('images')
+        if len(images) > 3:
+            image_err.append('Chọn tối đa ba ảnh') 
+        type_image =['image/jpeg', 'image/png','image/jpg']
+        for image in images:
+            if image.content_type not in type_image:
+                image_err.append('Chỉ nhận file jpeg ,png ,jpg')
+            if image.size >= 1024 *1024:
+                image_err.append(f"{image.name}cần nhỏ hơn 1 MB") 
+        if product_edit.is_valid() and not image_err:
+            image_paths =[]
+            product = product_edit.save(commit=False)
+            for image in images:
+                path =default_storage.save('product/'+image.name,image)
+                image_paths.append(path)
+            product.image = image_paths
+            product.save()
+            return JsonResponse({'success':'Tạo thành công',"redirect_url": reverse("product:my_product")})   
+    else:
+        product_edit=ProductForm(instance=product)
+    return render(request,'edit_product.html', {'product_edit':product_edit,'image_err':image_err,'product':product})
+def deleteProduct(request,id):
+    product= get_object_or_404(Product,id=id)
+    product.delete()
+    return redirect('product:my_product')
